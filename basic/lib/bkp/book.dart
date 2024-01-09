d7e8c3c885336abb422714c987a4017b91549a38
Tran Dinh Tu <d_tu@brycen.com.vn>
2024-01-09 14:44:00 +0700

    learn flutter basic

---
+++ basic/lib/bkp/book.dart
@@ -0,0 +1,50 @@
+class Book {
+  // late String name;
+  // late int year;
+  // Book(name, year) {
+  //   this.name = name;
+  //   this.year = year;
+  // }
+
+  //Book(this.name, this.year);
+  // String name;
+  // int year;
+  // Book({required this.name, required this.year});
+  // Book({required this.name, this.year = 2000});
+
+  // //final String _name;
+  // late String _name;
+  // late int year;
+  // static int age = 5;
+  // Book(this._name, this.year);
+
+  late String name;
+  late int year;
+  Book({required this.name, required this.year});
+
+  Book.bookNew(String name) {
+    this.name = name;
+    year = 2022;
+  }
+
+  String get getName => name;
+  set setName(String name) {
+    name = name;
+  }
+
+  static void showBookHello() {
+    print('Xin chao');
+  }
+}
+
+class BookFunny extends Book {
+  String type;
+
+  BookFunny(String name, int year, {this.type = 'Truyen vui'})
+      : super(name: name, year: year);
+
+  static void showBookFunnyHello() {
+    Book.showBookHello();
+    print('Xin chao BookFunny');
+  }
+}
---
+++ basic/lib/bkp/main.dart
@@ -0,0 +1,65 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'book.dart';
+
+void main(List<String> args) {
+  // dynamic dall = 'hello flutter!';
+  // print(dall);
+  // dall = 2;
+
+  // var vall = 'aaa';
+  // print(vall);
+  // // vall = 2;
+
+  // List<String> name = ['tu', 'ngan', 'thinh', 'thanh', 'duyet'];
+  // // print(name[0]);
+  // for (var mn in name) {
+  //   print(mn);
+  // }
+
+  // Map<String, int> obj = {'tu': 42, 'thinh': 34};
+  // // print(obj.keys);
+  // obj.forEach((key, value) {
+  //   print(key + ' ' + value.toString());
+  // });
+
+  // void showHello1(String name, int age) {
+  //   if (age > 40) {
+  //     print('Chào anh ' + name);
+  //   } else {
+  //     print('Chào em ' + name);
+  //   }
+  // }
+
+  // void showHello2({String name = 'tu', int age = 42}) {
+  //   if (age > 40) {
+  //     print('Chào anh ' + name);
+  //   } else {
+  //     print('Chào em ' + name);
+  //   }
+  // }
+
+  // showHello2(name: 'dat', age: 30);
+
+  // var MyBook = Book(name: 'Sport', year: 2000);
+  // var MyBook = Book(name: 'Sport');
+  // print(MyBook.name);
+  // print(MyBook.year);
+
+  // var MyBook = Book('IT', 2023);
+  //MyBook.showHello();
+  // print(MyBook.getName);
+  // MyBook.setName = 'Flutter';
+  // print(MyBook.getName);
+  // print(MyBook.year);
+
+  // var YourBook = Book.bookNew('Model');
+  // print(YourBook.getName);
+  // print(YourBook.year);
+  // // print(Book.age);
+  // Book.showHello();
+
+  var YourBook = BookFunny('ngụ ngôn', 2020);
+  print(YourBook.name);
+  BookFunny.showBookFunnyHello();
+}
